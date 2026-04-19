60335abfe67d21c92e9e20bc9fafc236d299c3a9
David-Kohn64 <159048038+David-Kohn64@users.noreply.github.com>
2026-04-19 00:19:22 -0400

    fixed client directory

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import { useState, useEffect, useRef } from 'react'
+
+function App() {
+  const wsRef = useRef(null)
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+  wsRef.current = new WebSocket('ws://localhost:3001')
+  wsRef.current.onmessage = (m) => {
+    setCounter(m.data)
+  }
+
+  }, [])
+
+  return (
+  <div>
+    <h1>Counter: {counter}</h1>
+    <button onMouseDown={() => wsRef.current.send('start')} onMouseUp={() => wsRef.current.send('stop')}>Hold me!</button>
+  </div>
+  )
+}
+
+export default App;
