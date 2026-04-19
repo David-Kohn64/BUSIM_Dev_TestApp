9c4aff7ca76e348b922e55e552c0df7d385c6c0b
thebiggonsv3 <zhang.ryan8976@gmail.com>
2026-04-19 16:11:58 -0400

    added button that displays a new number every time that button is clicked, rotating from a list

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { useState, useEffect, useRef } from 'react'
 function App() {
   const wsRef = useRef(null)
   const [counter, setCounter] = useState(0)
+  const numbers = [1, 2, 3, 10, 20, 30, -999]
+  const [displayIndex, setDisplayIndex] = useState(0)
 
   useEffect(() => {
   wsRef.current = new WebSocket('ws://localhost:3001')
@@ -17,6 +19,8 @@ function App() {
     <h1>Counter: {counter}</h1>
     <button onMouseDown={() => wsRef.current.send('start')} onMouseUp={() => wsRef.current.send('stop')}>Hold me!</button>
     <button onClick={() => wsRef.current.send('up') || setCounter(c => c + 1)}>Increment</button>
+    <div id="display">{numbers[displayIndex]}</div>
+    <button onClick={() => setDisplayIndex((prev) => (prev + 1) % numbers.length)}>Rotate Number</button>
   </div>
   )
 }
