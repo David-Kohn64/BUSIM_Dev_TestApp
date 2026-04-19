6ab9cdd6b30f35445db7416dc73d74961347c3e8
David-Kohn64 <159048038+David-Kohn64@users.noreply.github.com>
2026-04-18 23:51:12 -0400

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const http = require('http')
+const { clearInterval } = require('timers')
+const { WebSocketServer } = require('ws')
+
+const app = express()
+const server = http.createServer(app)
+const wss = new WebSocketServer({ server })
+
+let counter = 0
+let interval
+
+wss.on('connection', (ws) => {
+    console.log("User connected")
+
+    ws.on('message', (m) => {
+        if(m.toString() == "start"){
+            console.log("Started counter")
+            interval = setInterval(count, 1000)
+        }
+        else if(m.toString() == "stop"){
+            console.log("Stoppped counter")
+            clearInterval(interval)
+        }
+    })
+
+    ws.on('close', () => {
+        console.log("User closed")
+    })
+})
+
+function count() {
+    counter++
+    console.log(counter)
+    wss.clients.forEach((client) => {
+        client.send(counter)
+    })
+}
+
+const PORT = 3001
+
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
+
