6539c39b66d959b220271465fed77dd664a685da
Ace-of-Spades51 <143812804+Ace-of-Spades51@users.noreply.github.com>
2026-05-03 00:15:22 -0400

    reset and increment every 1000 ms done

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,11 @@ function App() {
   const numbers = [1, 2, 3, 10, 20, 30, -999]
   const [displayIndex, setDisplayIndex] = useState(0)
 
+  // Group 2, 2 constants
+  const [display2Val, incrementDisplay2] = useState(0)
+  const intervalRef = useRef(null)
+  const holdTimeoutRef = useRef(null)
+
   useEffect(() => {
   wsRef.current = new WebSocket('ws://localhost:3001')
   wsRef.current.onmessage = (m) => {
@@ -14,13 +19,39 @@ function App() {
 
   }, [])
 
+  // Increments every 1000 ms
+  useEffect(() => {
+  intervalRef.current = setInterval(() => {
+    incrementDisplay2(c => c + 1)
+  }, 1000)
+  return () => clearInterval(intervalRef.current)
+  }, [])
+
+  // When button is held, 5s timer starts
+  const holdStop = () => {
+    holdTimeoutRef.current = setTimeout(() => {
+      clearInterval(intervalRef.current)
+      intervalRef.current = null
+      console.log("Stopped auto increment")
+    }, 5000)
+  }
+
+  // Reset when button is released
+  const holdRelease = () => {
+    clearTimeout(holdTimeoutRef.current)
+  }
+
   return (
   <div>
     <h1>Counter: {counter}</h1>
     <button onMouseDown={() => wsRef.current.send('start')} onMouseUp={() => wsRef.current.send('stop')}>Hold me!</button>
     <button onClick={() => wsRef.current.send('up') || setCounter(c => c + 1)}>Increment</button>
+    <button onClick={() => wsRef.current.send('reset') || setCounter(0)}>Reset</button>
     <div id="display">{numbers[displayIndex]}</div>
     <button onClick={() => setDisplayIndex((prev) => (prev + 1) % numbers.length)}>Rotate Number</button>
+
+    <div id = "display2">{display2Val}</div>
+    <button onMouseDown={holdStop} onMouseUp={holdRelease}>Hold 5s to Stop</button>
   </div>
   )
 }
